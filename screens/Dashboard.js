90a178856d464270d69384977d8826af1a22e415
abhishekchakram <abhishek@metfolio.com>
2023-04-01 12:22:38 +0530

    added gps

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -2,12 +2,129 @@ import {StyleSheet, View, Text, Dimensions} from 'react-native';
 import Tiles from '../components/Tiles';
 import {Colors} from '../constants/Colors';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {useEffect, useState} from 'react';
 
+import Geolocation from '@react-native-community/geolocation';
 import {useSelector} from 'react-redux';
 
 function Dashboard() {
   const currentDate = new Date().toLocaleDateString();
 
+  const [
+    currentLongitude,
+    setCurrentLongitude
+  ] = useState('...');
+  const [
+    currentLatitude,
+    setCurrentLatitude
+  ] = useState('...');
+  const [
+    locationStatus,
+    setLocationStatus
+  ] = useState('');
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      if (Platform.OS === 'ios') {
+        getOneTimeLocation();
+        subscribeLocationLocation();
+      } else {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: 'Location Access Required',
+              message: 'This App needs to Access your location',
+            },
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //To Check, If Permission is granted
+            getOneTimeLocation();
+            subscribeLocationLocation();
+          } else {
+            setLocationStatus('Permission Denied');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      }
+    };
+    requestLocationPermission();
+    return () => {
+      Geolocation.clearWatch(watchID);
+    };
+  }, [Geolocation]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getOneTimeLocation();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const getOneTimeLocation = () => {
+    setLocationStatus('Getting Location ...');
+    Geolocation.getCurrentPosition(
+      //Will give you the current location
+      (position) => {
+        setLocationStatus('You are Here');
+
+        //getting the Longitude from the location json
+        const currentLongitude = 
+          JSON.stringify(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = 
+          JSON.stringify(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+        
+        //Setting Longitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      (error) => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 30000,
+        maximumAge: 1000
+      },
+    );
+  };
+
+  const subscribeLocationLocation = () => {
+    watchID = Geolocation.watchPosition(
+      (position) => {
+        //Will give you the location on location change
+        
+        setLocationStatus('You are Here');
+        console.log(position);
+
+        //getting the Longitude from the location json        
+        const currentLongitude =
+          JSON.stringify(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = 
+          JSON.stringify(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Latitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      (error) => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 1000
+      },
+    );
+  };
   const data = useSelector(state => state.data);
   // console.log(data);
 
@@ -47,7 +164,22 @@ function Dashboard() {
           <Tiles dataname="0 hr 0 min" unit="Upload" />
         </View>
       </View>
-
+      <Text
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 16,
+            }}>
+            Longitude: {currentLongitude}
+          </Text>
+          <Text
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 16,
+            }}>
+            Latitude: {currentLatitude}
+          </Text>
       <View style={styles.container2}>
         <View style={styles.tileCover}>
           <Tiles data={vol} unit="V" dataname="Voltage" />
